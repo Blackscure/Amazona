ec0c9b0ac9a0faf74f188bd64c4ad107723cad20
blackscure <Wekesabuyahi@gmail.com>
2020-11-05 13:12:23 +0300

    js file created with objects inside

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function App() {
   return (
-    <div class="grid-container">
-    <header class="row">
+    <div className="grid-container">
+    <header className="row">
         <div>
-            <a class="brand" href="index.html">amazona</a>
+            <a className="brand" href="index.html">amazona</a>
         </div>
         <div>
             <a href="cart.html">Cart</a>
@@ -13,135 +13,135 @@ function App() {
         </div>
     </header> 
     <main>
-         <div class="row center">
-             <div class="card">
+         <div className="row center">
+             <div className="card">
                  <a href="product.html">
                      
-                     <img class = "medium" src="./images/p1.jpg" alt="product" />
+                     <img className = "medium" src="./images/p1.jpg" alt="product" />
                  </a>
-                 <div class="card-body">
+                 <div className="card-body">
                      <a href="product.html">
                          <h2>Nike slim shirts</h2>
                      </a>
-                     <div class="rating">
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <div class="price"> kes 1900</div>
+                     <div className="rating">
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <div className="price"> kes 1900</div>
                      </div>
                  </div>
              </div>
 
-             <div class="card">
+             <div className="card">
                  <a href="product.html">
                      
-                     <img class = "medium" src="./images/p2.jpg" alt="product" />
+                     <img className = "medium" src="./images/p2.jpg" alt="product" />
                  </a>
-                 <div class="card-body">
+                 <div className="card-body">
                      <a href="product.html">
                          <h2>Nike slim shirts</h2>
                      </a>
-                     <div class="rating">
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <div class="price"> kes 1900</div>
+                     <div className="rating">
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <div className="price"> kes 1900</div>
                      </div>
                  </div>
              </div>
 
-             <div class="card">
+             <div className="card">
                  <a href="product.html">
                      
-                     <img class = "medium" src="./images/p3.jpg" alt="product" />
+                     <img className = "medium" src="./images/p3.jpg" alt="product" />
                  </a>
-                 <div class="card-body">
+                 <div className="card-body">
                      <a href="product.html">
                          <h2>Nike slim shirts</h2>
                      </a>
-                     <div class="rating">
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <div class="price"> kes 1900</div>
+                     <div className="rating">
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <div className="price"> kes 1900</div>
                      </div>
                  </div>
              </div>
 
-             <div class="card">
+             <div className="card">
                  <a href="product.html">
                      
-                     <img class = "medium" src="./images/p4.jpg" alt="product" />
+                     <img className = "medium" src="./images/p4.jpg" alt="product" />
                  </a>
-                 <div class="card-body">
+                 <div className="card-body">
                      <a href="product.html">
                          <h2>Nike slim shirts</h2>
                      </a>
-                     <div class="rating">
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <div class="price"> kes 1900</div>
+                     <div className="rating">
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <div className="price"> kes 1900</div>
                      </div>
                  </div>
              </div>
 
-             <div class="card">
+             <div className="card">
                  <a href="product.html">
                      
-                     <img class = "medium" src="./images/p5.jpg" alt="product" />
+                     <img className = "medium" src="./images/p5.jpg" alt="product" />
                  </a>
-                 <div class="card-body">
+                 <div className="card-body">
                      <a href="product.html">
                          <h2>Nike slim shirts</h2>
                      </a>
-                     <div class="rating">
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <div class="price"> kes 1900</div>
+                     <div className="rating">
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <div className="price"> kes 1900</div>
                      </div>
                  </div>
              </div>
 
-             <div class="card">
+             <div className="card">
                  <a href="product.html">
                      
-                     <img class = "medium" src="./images/p6.jpg" alt="product" />
+                     <img className = "medium" src="./images/p6.jpg" alt="product" />
                  </a>
-                 <div class="card-body">
+                 <div className="card-body">
                      <a href="product.html">
                          <h2>Nike slim shirts</h2>
                      </a>
-                     <div class="rating">
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <span><i class="fa fa-star"></i></span>
-                         <div class="price"> kes 1900</div>
+                     <div className="rating">
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <span><i className="fa fa-star"></i></span>
+                         <div className="price"> kes 1900</div>
                      </div>
                  </div>
              </div>
          </div>
     </main>
-    <footer class="row center">
+    <footer className="row center">
          All right reserved
     </footer>
  </div>
---
+++ frontend/src/data.js
@@ -0,0 +1,15 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Nike slime shirt',
+            category: 'Shirt',
+            image: '/images/p1.jpg',
+            price: 1900,
+            barnd: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+    ],
+};
