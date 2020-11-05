0f1cd134da0479c1760f2ca44d14fcad1a317557
blackscure <Wekesabuyahi@gmail.com>
2020-11-05 22:42:06 +0300

    product file created in components folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Product from './components/Product';
+
 import data from './data';
 
 function App() {
@@ -6,7 +8,7 @@ function App() {
     <div className="grid-container">
     <header className="row">
         <div>
-            <a className="brand" href="/">amazona</a>
+            <a className="brand" href="/">Hamanzi</a>
         </div>
         <div>
             <a href="/cart">Cart</a>
@@ -15,34 +17,9 @@ function App() {
     </header> 
     <main>
          <div className="row center">
-             {
-                 data.products.map(product => (
-                    <div key={product._id} className="card">
-                    <a href="`/product/${product._id}`">
-                        
-                        <img
-                         className = "medium" 
-                         src={product.image}
-                         alt={product.name}
-                          />
-                    </a>
-                    <div className="card-body">
-                    <a href="`/product/${product._id}`">
-                 <h2>{product.name}</h2>
-                        </a>
-                        <div className="rating">
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                 <div className="price">${product.price}</div>
-                        </div>
-                    </div>
-                </div>
-                 ))
-             }
+             {data.products.map(product => (
+                    <Product key={product._id} product={product}></Product>
+                 ))}
              
          </div>
     </main>
